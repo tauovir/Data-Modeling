6c0072736f0cd4173e94673b2375f297d6b00434
tauovir <taukir707@gmail.com>
2023-08-15 17:30:39 +0530

    update

---
+++ propel/python/matrix_func.py
@@ -0,0 +1,26 @@
+
+
+MATRIX = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
+
+def flatten():
+    # output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    pass
+
+def row_addtion():
+    # list comprehension
+    # output:[6, 15, 24]
+    pass
+
+def col_addtion(slf):
+    # output: [12, 15, 18]
+    pass
+
+def row_reverse(sef):
+    # Result: [[3, 2, 1], [6, 5, 4], [9, 8, 7]]
+    pass
+
