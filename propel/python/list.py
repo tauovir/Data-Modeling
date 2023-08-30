59269e04bcaf1f10752815a44b92191eb41396b3
tauovir <taukir707@gmail.com>
2023-08-31 02:48:13 +0530

    update

---
+++ propel/python/list.py
@@ -0,0 +1,9 @@
+l1 = ["mango","orange","apple"]
+l2 = ["Red","green", "black"]
+
+def add_list():
+	# output: ['mango', 'orange', 'apple', 'Red', 'green', 'black']
+    pass
+
+def slice_list():
+    # output: ['orange', 'apple', 'Red', 'green']
