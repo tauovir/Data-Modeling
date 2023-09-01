179483973d922969da40b71a81b32cb75b45ec41
tauovir <taukir707@gmail.com>
2023-09-01 20:51:04 +0530

    update

--- propel/python/matrix_func.py
+++ propel/python/matrix_func.py
@@ -13,31 +13,38 @@ def row_addtion():
     # output:[6, 15, 24]
     pass
 
-def col_addtion():
-    # output: [12, 15, 18]
-    pass
-
 def row_reverse():
     # Result: [[3, 2, 1], [6, 5, 4], [9, 8, 7]]
     pass
 
+def col_addtion():
+    # output: [12, 15, 18]
+    pass
 
 
-Data fram:
+df_emp:
 emp_name country salary	 bonus emp_dept_id
 A	  india	 2000	300			1
 B     india  3000   500			1
 C      US    2500    250 		2
 D      UK    4000    0			2
 
-Department:
+df_dept:
 dept_id dept_name
 1			IT
 2			Sales
 3			HR
 
+
+1: Display total salary by country
+2: Fetch emp_name, salary, dept_name whenre country is India
+3: write dataframe into csv file,.
+
+SQL:
+1: Display total salary by country
+2: Fetch emp_name, salary, dept_name whenre country is India
+
+
+4: Fetch emp_name, salary, dept_name
 1: Diplay emp_name and salary
 2: Display total employee in each country
-3: Display total salary by country
-4: Fetch emp_name, salary, dept_name
-5: Fetch emp_name, salary, dept_name whenre country is India
